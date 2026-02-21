1f398ba932058ce40d9f880c0b5cb44344dc93c6
kavakenedi-sketch <kavakenedi@gmail.com>
2026-02-21 23:38:25 +0300

    Update pepe.py

--- pepe.py
+++ pepe.py
@@ -8,7 +8,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 from telegram.constants import ChatMemberStatus
 from telegram.error import TelegramError
 
-TOKEN = "8019928524:AAEA_SYAPK21AQmWR1aAB_u6dqvJq4vUaW8"
+TOKEN = "8395660188:AAHBTjggX3Nwfx7Xg01uS2cQFb9mgEYM4iY"
 DB_PATH = "stats.db"
 TIMEZONE = pytz.timezone("Europe/Moscow")
 
